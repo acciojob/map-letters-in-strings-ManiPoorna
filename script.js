bfefb8354e2c67982cda6e050cee37f52a05a18c
Mani Poorna <118202451+ManiPoorna@users.noreply.github.com>
2023-09-15 11:22:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+//your JS code here. If required.
+const word = prompt();
+alert(mapLetters(word))
+
+function mapLetters(word) {
+	let ansObj = {};
+	for (let letter of word) {
+		ansObj[`${letter}`] = [];
+	}
+	for (let i = 0; i < word.length; i++) {
+		ansObj[`${word[i]}`].push(i);
+	}
+	return ansObj
+}
